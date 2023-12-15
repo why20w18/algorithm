01310a8eb075341ad772515238bc6401bd901e1b
why20w18 <enco3162@gmail.com>
2023-12-15 20:43:49 +0300

    girilen araliktaki asal sayilari tespit

--- basicAlgorithm/araliktakiAsallar.java
+++ basicAlgorithm/araliktakiAsallar.java
@@ -1,45 +1,47 @@
 package basicAlgorithm;
 import java.util.Scanner;
 
-public class araliktakiAsallar {
-    
-    public static void asalBul(int x, int y) {
-     int min = (x < y) ? x : y;
-     int max = (x < y) ? y : x;
 
-    System.out.println("Araliktaki asal sayilar:");
-    for (int i = min; i <= max; i++) {
-        if (asalMi(i)) {
-            System.out.println(i);
+public class araliktakiAsallar{
+    
+    public static void araliktaAra(int x , int y){
+        
+        int min = (x < y) ? x:y;
+        int max = (x > y) ? x:y;
+        
+        for(int i = min ; i <= max ; i++){
+            boolean asalFlag = true;
+            
+            if(i < 2){
+                asalFlag = false;
+            }
+            else{
+                
+                for(int j = 2 ; j < i ; j++){
+                    
+                    if(i % j == 0){
+                        asalFlag = false;
+                        break;
+                    }   
+                }
+            }
+            if(asalFlag){
+                System.out.print(i+" ");
+            }
         }
     }
-}
-
-public static boolean asalMi(int sayi) {
-    if (sayi < 2) {
-        return false;
-    }
-    for (int j = 2; j <= Math.sqrt(sayi); j++) {
-        if (sayi % j == 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
-    
-    
     
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
         
-        System.out.print("Aralik girin:");
+        System.out.print("Aralik girin:\n>");
         int x = scanner.nextInt();
+        
+        System.out.print(">");
         int y = scanner.nextInt();
         
-        asalBul(x,y);
-        
+        araliktaAra(x, y);
         
     }
     
