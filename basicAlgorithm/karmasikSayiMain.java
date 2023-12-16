3b9a2e68cd06cb6beaaaa9aa6fbd600297c9d2e5
why20w18 <enco3162@gmail.com>
2023-12-16 15:48:09 +0300

    karmasik sayi hesaplayici

--- basicAlgorithm/karmasikSayiMain.java
+++ basicAlgorithm/karmasikSayiMain.java
@@ -1,43 +1,53 @@
 package basicAlgorithm;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
+//Sanal kisim pozitif mi negatif mi ?
 
-
-public class karmasikSayiMain {
+public class karmasikSayiMain{
     
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Yapacaginiz islemi secin:"
+                + "\n1-Toplama\n"
+                + "2-Cikartma\n"
+                + "\nIslem:");
+        int secim = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("ilk karmasik sayi:");
+        String ilk = scanner.nextLine();
         
-        System.out.print("karmasik sayi girin:");
-        String sayBir = scanner.nextLine();
+        System.out.print("ikinci karmasik sayi:");
+        String iki = scanner.nextLine();
         
-        System.out.print("karmasik sayi girin:");
-        String sayIki = scanner.nextLine();
+        hesaplamaSinif hesaplayici = new hesaplamaSinif(ilk,iki);
         
-        karmasikSayi say1 = new  karmasikSayi(sayBir);
-        karmasikSayi say2 = new  karmasikSayi(sayIki);
-        
-        hesaplama hesapla = new hesaplama(sayBir, sayIki);
-        
-        hesapla.topla().ekran();
+        hesaplayici.islemler(secim).ekran(secim);
         
     }
+    
 }
+
 
 class karmasikSayi{
     private double reel;
     private double sanal;
     
     public karmasikSayi(double reel , double sanal){
-        this.reel =  reel;
-        this.sanal = sanal;
+       this.reel = reel;
+       this.sanal = sanal;
+        
     }
-    //Mainde alinan stringi buraya gonderecegim
-    public karmasikSayi(String karmasikSayiButun){
-        String karmasikSayi[] = karmasikSayiButun.split("[+\\-\\*\\/]");
-        this.reel  = Double.parseDouble(karmasikSayi[0]);
-        this.sanal = Double.parseDouble(karmasikSayi[1].substring(0, karmasikSayi.length-1));  
+    public karmasikSayi(String karmasikGirdi){
+        //metni reel ve sanal kisim olacak sekilde bolmeliyim
+        String girdiBolunmus[] = karmasikGirdi.split("[+\\-]");
+        //+ - gordugu yerden bolecek
+        //bolunen kisimlari ata
+        
+        this.reel = Double.valueOf(girdiBolunmus[0]);
+        this.sanal = Double.valueOf(girdiBolunmus[1].substring(0, girdiBolunmus[1].length()-1));
+        
     }
 
     public double getReel() {
@@ -48,41 +58,74 @@ class karmasikSayi{
         return sanal;
     }
     
-    void ekran(){
-        System.out.printf(getReel()+" + "+getSanal()+"i");
-        
-    }
+   void ekran(int secim) {
+        switch (secim) {
+            case 1 -> {
+                System.out.print(getReel());
+                if (getSanal() > 0) {
+                    System.out.println("+" + getSanal() + "i");
+                } else if (getSanal() < 0) {
+                    System.out.println("-"+getSanal() + "i");
+                }
+            }
+            case 2 -> {
+                System.out.print(getReel());
+                if (getSanal() > 0) {
+                    System.out.println("+" + getSanal() + "i");
+                } else if (getSanal() < 0) {
+                    System.out.println(getSanal() + "i");
+                }
+            }
+            default -> System.out.print("");
+        }
+}
+// 4 3
+   //2 -6
     
 }
 
-
-class hesaplama{
+class hesaplamaSinif{
     
-    private karmasikSayi say1;
-    private karmasikSayi say2;
+    private karmasikSayi sayi1;
+    private karmasikSayi sayi2;
     
-    public hesaplama(String say1 , String say2){
-        this.say1 = new karmasikSayi(say1);
-        this.say2 = new karmasikSayi(say2);
+    public hesaplamaSinif(String sayi1 , String sayi2){
+        
+        this.sayi1 = new karmasikSayi(sayi1);
+        this.sayi2 = new karmasikSayi(sayi2);
         
     }
     
-    public karmasikSayi topla(){
+    karmasikSayi islemler(int secim){
         
-        double reel = say1.getReel() + say2.getReel();
-        double sanal = say1.getSanal() + say2.getSanal();
-        
-        return new karmasikSayi(reel, sanal);
+        switch(secim){
+            
+            case 1->{
+                if(sayi1.getSanal() > 0 && sayi2.getSanal() > 0){
+                    
+                double reel = sayi1.getReel() + sayi2.getReel();
+                double sanal = sayi1.getSanal() + sayi2.getSanal();
+                return new karmasikSayi(reel, sanal);
+                }
+                
+                
+               
+                
+            }    
+            case 2->{
+                double reel = sayi1.getReel()-sayi2.getReel();
+                double sanal = sayi1.getSanal()-sayi2.getSanal();
+                return new karmasikSayi(reel, sanal);
+            }
+                
+           default->{
+               System.out.println("sadece 1 veya 2 girebilirsin !");
+               break;
+           }
+            
+        }
+        return new karmasikSayi(0, 0);
     }
-    
-    public karmasikSayi cikartma(){
-        double reel = say1.getReel() - say2.getReel();
-        double sanal = say1.getSanal() - say2.getSanal();
-        
-        return new karmasikSayi(reel, sanal);
-        
-    }
-    
     
     
 }
