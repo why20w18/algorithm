c34474164d8b02639133cfa1305b32a7b6b7bde8
why20w18 <enco3162@gmail.com>
2023-12-17 14:15:41 +0300

    Amazon turkiyede cok satan en fazla talep edilen en performansli ve yeni cikan kitaplari listeleyen program

--- streams_Web/amazonBestSellerBook_Jsoup.java
+++ streams_Web/amazonBestSellerBook_Jsoup.java
@@ -1,7 +1,6 @@
 package streams_Web;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -10,17 +9,51 @@ import org.jsoup.select.Elements;
 public class amazonBestSellerBook_Jsoup {
 
     public static void main(String[] args) throws Exception {
+        int cx = 1;
+        Scanner scanner = new Scanner(System.in);
+        String secim = "";
+        while(!false){
+        
+        System.out.print("<----------------------------GUNCEL AMAZON VERI CEKIP LISTELEYICI---------------------------->"
+                + "\nCALISTIRILMA SAYISI: "+cx
+                + "\nSECENEKLER:\n"
+                + ">>cok satan\n"
+                + ">>yeni cikan\n"
+                + ">>en talep edilen\n"
+                + ">>en iyi performans"
+                + "\n>>cikis"
+                + "\nSON CALISAN KOMUT:"+secim+" YUKARIDA LISTELENMISTIR !"+"\n\n>>");
+        secim = scanner.nextLine().toLowerCase().replace(" ", "");
+        String kitapBaslik = "",yazarBaslik = "YAZAR ISIMLERI";
+        
 
   Document dokuman = null;
-
-  dokuman = Jsoup.connect("https://www.amazon.com.tr/gp/bestsellers/books").get();
-
+  if(secim.equals("coksatan")){
+        dokuman = Jsoup.connect("https://www.amazon.com.tr/gp/bestsellers/books").get();
+        kitapBaslik = "AMAZON EN COK SATAN KITAPLAR";
+  }
+  else if(secim.equals("yenicikan")){
+        dokuman = Jsoup.connect("https://www.amazon.com.tr/gp/new-releases/books/ref=zg_bs_tab_t_books_bsnr").get();
+        kitapBaslik = "AMAZON YENI CIKAN KITAPLAR";
+  }
+  else if(secim.equals("entalepedilen")){
+        dokuman = Jsoup.connect("https://www.amazon.com.tr/gp/most-wished-for/books/ref=zg_bsnr_tab_t_books_mw").get();
+        kitapBaslik = "AMAZON EN COK TALEP EDILEN KITAPLAR";
+  }
+  else if(secim.equals("eniyiperformans")){
+        dokuman = Jsoup.connect("https://www.amazon.com.tr/gp/movers-and-shakers/books/ref=zg_mw_tab_t_books_bsms").get();
+        kitapBaslik = "AMAZON EN IYI PERFORMANS GOSTEREN KITAPLAR";
+  }
+  else if(secim.equals("cikis")){
+      System.out.println("Program sonlandirildi ...");
+      System.exit(0);
+  }
   Elements element = dokuman.select("div._cDEzb_p13n-sc-css-line-clamp-1_1Fn1y"); 
 
   int satirNo = 0;
   int numaralandir = 1;
         
-        System.out.println("<---------------------AMAZON EN FAZLA SATAN KITAPLAR GUNCEL LISTE--------------------->");
+        System.out.println("<---------------------"+kitapBaslik+"--------------------->");
         for(Element y : element){
             
             if(satirNo % 2 == 0){
@@ -32,7 +65,7 @@ public class amazonBestSellerBook_Jsoup {
         }
         satirNo = 0;
         numaralandir = 1;
-        System.out.println("<----------------------------AMAZON EN FAZLA SATAN KITAPLARIN YAZARLARI GUNCEL LISTE---------------------------->");
+        System.out.println("<----------------------------"+yazarBaslik+"---------------------------->");
         for(Element x : element){
             
             if(satirNo % 2 != 0){
@@ -43,8 +76,8 @@ public class amazonBestSellerBook_Jsoup {
             satirNo++;
         }
   
-  
-        
-    }
+    cx++;
+    }//while  
+    } 
     
 }
