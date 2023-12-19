7d081d27877c143f8c0e5d40e020419cdc2d5ace
why20w18 <enco3162@gmail.com>
2023-12-19 17:03:26 +0300

    ilk buble sort

---
+++ sortAlgorithm/bubbleSort.java
@@ -0,0 +1,45 @@
+package sortAlgorithm;
+import java.util.Scanner;
+
+
+public class bubbleSort {
+    
+    public static int [] buble(int [] dizi){
+        int temp = 0;
+        for(int i = 0 ; i < dizi.length ; i++){
+            
+            for(int j = 0 ; j < dizi.length-1 ; j++){
+                
+                if(dizi[j] > dizi[j+1]){
+                    
+                    temp = dizi[j];
+                    dizi[j] = dizi[j+1];
+                    dizi[j+1] = temp;
+                    
+                }
+                
+            }
+        }
+        return dizi;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Kac terimli random dizi olusturulsun :");
+        int terim = scanner.nextInt();
+        
+        int[] dizi = new int[terim];
+        for(int i = 0 ; i < terim ; i++) dizi[i] = (int) (Math.random() * 56);
+        
+        System.out.println("RASTGELE DIZI");
+        for(int i : dizi) System.out.print(i+" ");
+        
+        System.out.println("\nSIRALANMIS DIZI");
+        int[] sonuc = buble(dizi);
+        
+        for(int j : sonuc) System.out.print(j+" ");
+        
+    }
+    
+}
