362408498e03d40202847ab0ebcf3d6691bb6f4f
why20w18 <enco3162@gmail.com>
2023-12-20 00:37:31 +0300

    wikiCLI tamamlandi komut satiri tanimlandi

--- streams_Web/wikiCLI.java
+++ streams_Web/wikiCLI.java
@@ -7,11 +7,22 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class wikiCLI {
+import java.util.ArrayList;
+import java.util.Scanner;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+public class WikiCLI {
+    
+    public static final int SATIR_UZUNLUGU = 120; //120 karakter cmd size olarak
+    
     public static void main(String[] args) throws Exception {
+        
+        StringBuilder sb = new StringBuilder();
+        
         Scanner scanner = new Scanner(System.in);
-
         String viki = "https://tr.wikipedia.org/w/index.php?search=";
         String araSon = "&title=%C3%96zel:Ara&profile=advanced&fulltext=1&ns0=1";
         String aranacakKelime = "";
@@ -21,16 +32,31 @@ public class wikiCLI {
         int no = 1;
 
         while (!aranacakKelime.equals("cikis")) {
-            System.out.println("\n<----------------->\nGirilen son komut : "+aranacakKelime);
-            System.out.println("WikiCLI<Aramak istediginizi girin>:");
+            System.out.println("\n<------------------------------WikiCLI------------------------------>\ngirilen son komut : "+aranacakKelime);
+            System.out.print("\nArayin\n~$");
             aranacakKelime = scanner.nextLine();
             aranacakText = aranacakKelime.replaceAll(" ", "\\+");
             
             
-             if(aranacakKelime.contains("sil")){
+             if(aranacakKelime.equals("sil")){
                 gelenSayfaURL.clear();
                 continue;
             }
+             if(aranacakKelime.equals("exit")){
+                 System.out.println("\nWikiCLI terminated !\nPROGRAM SONLANDIRILDI...");
+                 break;
+             }
+             if(aranacakKelime.equals("help")){
+                 System.out.println("###KOMUT LISTESI###\n"
+                         + "\nSECIM EKRANI KOMUTLARI:"
+                         + "\n@sec NO : aranan anahtar sonucu siralanan sayfalara gecis saglar"
+                         + "\n@exit   : programi sonlandirir"
+                         + "\nANA EKRAN KOMUTLARI"
+                         + "\n~$sil  : sayfalar arasi gecislerde eski sayfa gosterilmeye devam ederse"
+                         + "\n~$help : komut sayfasini baslatir"
+                         + "\n~$exit : programi sonlandirir");
+                 continue;
+             }
             
             Document doc = Jsoup.connect(viki + aranacakText + araSon).get();
             Elements element = doc.select("div.mw-search-result-heading");
@@ -44,7 +70,7 @@ public class wikiCLI {
             }
 
             // Kullanýcýnýn giriþini bir deðiþkende saklayýn ve kontrol edin
-            System.out.print("Acilmasi istenen sayfa >>>");
+            System.out.print("Acilmasi istenen sayfa @");
             String kullaniciGirisi = scanner.nextLine();
             if (kullaniciGirisi.contains("sec")) {
 
@@ -53,14 +79,31 @@ public class wikiCLI {
                 doc = Jsoup.connect(URL2).get();
                 element = doc.select("div.mw-content-ltr.mw-parser-output");
 
-                for (Element y : element) {
-                    System.out.println(y.text());
-                }
+             for (Element y : element) {
+    String satirlar[] = y.text().split("\\s+"); // diziye bosluklara gore split edip attim
+    
+    // y.text'e mudahale edemiyoruz o yuzden hepsini bir diziye kelime kelime atalim
+    for (String w : satirlar) {
+        if (sb.length() > 0 && sb.length() + w.length() > SATIR_UZUNLUGU) {
+            System.out.println(sb.toString());
+            sb.setLength(0); // sb'yi sýfýrlayalým
+        }
+        sb.append(w).append(" "); // kelimeyi ekleyelim
+    }
+    
+    // Döngü bittikten sonra kalanlarý yazdýralým
+    if (sb.length() > 0) {
+        System.out.println(sb.toString());
+        sb.setLength(0); // sb'yi sýfýrlayalým
+    }
+                    gelenSayfaURL.clear();
+             }
+
             }
            
-            if(kullaniciGirisi.equals("c")){
-                System.out.println("Secim Atlandi ...");
-                continue;
+            if(kullaniciGirisi.equals("exit")){
+                System.out.println("\nWikiCLI terminated !\nPROGRAM SONLANDIRILDI...");
+                break;
             
             }
 
