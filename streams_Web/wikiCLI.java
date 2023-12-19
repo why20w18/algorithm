175446f046770e01e4984d8d665601752ff6e08f
why20w18 <enco3162@gmail.com>
2023-12-19 20:36:00 +0300

    arraylist yardimiyla veri cekiyor div bug tek satirda hersey

--- streams_Web/wikiCLI.java
+++ streams_Web/wikiCLI.java
@@ -7,66 +7,62 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-
-
-
 public class wikiCLI {
 
-    public static String bin(int no){
-        
-        
-        return "test";
-    }
-    
-    
-    
-
-    
-        //private static final int ORTALAMA_UZUNLUK = 150;
-        
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        
+
         String viki = "https://tr.wikipedia.org/w/index.php?search=";
         String araSon = "&title=%C3%96zel:Ara&profile=advanced&fulltext=1&ns0=1";
         String aranacakKelime = "";
         String aranacakText = "";
-        
-        ArrayList<String> gelenSayfaURL = new ArrayList<String>();
+
+        ArrayList<String> gelenSayfaURL = new ArrayList<>();
         int no = 1;
 
-        while(!aranacakKelime.equals("cikis")){
+        while (!aranacakKelime.equals("cikis")) {
+            System.out.println("\n<----------------->\nGirilen son komut : "+aranacakKelime);
+            System.out.println("WikiCLI<Aramak istediginizi girin>:");
+            aranacakKelime = scanner.nextLine();
+            aranacakText = aranacakKelime.replaceAll(" ", "\\+");
             
-        System.out.println("WikiCLI<Aramak istediginizi girin>:");
-        aranacakKelime = scanner.nextLine();
-        aranacakText = aranacakKelime.replaceAll(" ", "\\+");
-        //div.mw-search-results-container >> anasayfadaki metinler
+            
+             if(aranacakKelime.contains("sil")){
+                gelenSayfaURL.clear();
+            }
+            
+            Document doc = Jsoup.connect(viki + aranacakText + araSon).get();
+            Elements element = doc.select("div.mw-search-result-heading");
+            
 
-        //System.out.println(viki+aranacakText+araSon);
-        
+            for (Element x : element) {
+                System.out.println(no + "-" + x.text());
+                no++;
+                String urlSonFormat = x.text().replaceAll(" ", "_");
+                gelenSayfaURL.add("https://tr.wikipedia.org/wiki/" + urlSonFormat);
+            }
 
-        Document doc = Jsoup.connect(viki+aranacakText+araSon).get();
-        Elements element = doc.select("div.mw-search-result-heading");
-        
-        for (Element x : element) {
-            System.out.println(no+"-"+x.text());
-            no++;
-            String urlSonFormat = x.text().replaceAll(" ", "_");
-            gelenSayfaURL.add("https://tr.wikipedia.org/wiki/"+urlSonFormat);
-            if(scanner.hasNextLine() && scanner.nextLine().contains("sec")){ //string olarak sec ile baslayan birsey girerse
-                //sec ve girilen int deger ayrilmali
-                String komut = scanner.nextLine();
-                int girilenNo = Integer.parseInt(komut)-1; //son indexi cikardik
-                
-                
-            }//scanner.hasNextLine() && scanner.nextLine().equals("sec")
+            // Kullanýcýnýn giriþini bir deðiþkende saklayýn ve kontrol edin
+            System.out.print("Acilmasi istenen sayfa >>>");
+            String kullaniciGirisi = scanner.nextLine();
+            if (kullaniciGirisi.contains("sec")) {
+
+                int girilenNo = Integer.parseInt(kullaniciGirisi.split(" ")[1]) - 1;
+                String URL2 = gelenSayfaURL.get(girilenNo);
+                doc = Jsoup.connect(URL2).get();
+                element = doc.select("div.mw-content-ltr.mw-parser-output");
+
+                for (Element y : element) {
+                    System.out.println(y.text());
+                }
+            }
+           
+            if(kullaniciGirisi.equals("c")){
+                System.out.println("Secim Atlandi ...");
+                continue;
+            }
+
+            no = 1;
         }
-        gelenSayfaURL.clear(); //baðlý listeyi bosalttik
-        no = 1;
-        
-            
-        }//while
-        
-        
     }
 }
