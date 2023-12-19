a62519fdd164c0a1a00a692476b463564d68e6ec
why20w18 <enco3162@gmail.com>
2023-12-19 19:29:39 +0300

    div tespit edildi aramada problem var

---
+++ streams_Web/wikiCLI.java
@@ -0,0 +1,44 @@
+package streams_Web;
+
+import java.util.Scanner;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+
+
+public class wikiCLI {
+    
+        //private static final int ORTALAMA_UZUNLUK = 150;
+        
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        
+        String viki = "https://tr.wikipedia.org/w/index.php?search=";
+        String araSon = "&title=%C3%96zel:Ara&profile=advanced&fulltext=1&ns0=1";
+        String aranacakKelime = "";
+        StringBuilder sb = new StringBuilder();
+        int no = 1;
+        while(!aranacakKelime.equals("cikis")){
+            
+        System.out.println("WikiCLI<Aramak istediginizi girin>:");
+        aranacakKelime = scanner.nextLine();
+        
+        //div.mw-search-results-container >> anasayfadaki metinler
+        
+        Document doc = Jsoup.connect(viki+aranacakKelime+araSon).get();
+        Elements element = doc.select("div.mw-search-result-heading");
+        
+        for (Element x : element) {
+            System.out.println(no+"-"+x.text());
+            no++;
+        }
+        
+            
+        }
+        
+        
+    }
+}
