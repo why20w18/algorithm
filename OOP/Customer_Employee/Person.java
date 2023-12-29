9ffef4ec841e33ce008e491fc760432b5bf40e47
why20w18 <enco3162@gmail.com>
2023-12-29 17:19:26 +0300

    getter ve setter eklendi

--- OOP/Customer_Employee/Person.java
+++ OOP/Customer_Employee/Person.java
@@ -7,6 +7,30 @@ public class Person {
     private int id;
     private String isim;
     private String soyIsim;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+
+    public String getSoyIsim() {
+        return soyIsim;
+    }
+
+    public void setSoyIsim(String soyIsim) {
+        this.soyIsim = soyIsim;
+    }
     
     
 }
