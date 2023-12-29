777c83ccd92b0cd03e0c9203b8045c95eb3d7c4c
why20w18 <enco3162@gmail.com>
2023-12-29 23:46:40 +0300

    tek attributes var isim override ses yapildi

--- OOP/hayvanOverrideInheritance/Inek.java
+++ OOP/hayvanOverrideInheritance/Inek.java
@@ -1,5 +1,21 @@
 package OOP.hayvanOverrideInheritance;
 
-public class Inek {
+public class Inek extends Hayvan{
     
+    private String isim;
+    
+    public Inek(String isim,String renk , String habitat , String beslenmeSekli , int ayakSayisi){
+        super(isim,renk, habitat, beslenmeSekli, ayakSayisi);
+        this.isim = isim;
+    }
+    
+    @Override
+    public void ses(){
+        System.out.println("Inek Moo");
+    }
+
+    public String getIsim() {
+        return isim;
+    }
+
 }
