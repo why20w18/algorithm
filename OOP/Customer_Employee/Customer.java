66be421880ff87708f0c5e0143a506c7eb4a6038
why20w18 <enco3162@gmail.com>
2023-12-29 17:08:43 +0300

    contribute eklendi ve ortak olanlar kaldirildi

--- OOP/Customer_Employee/Customer.java
+++ OOP/Customer_Employee/Customer.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package OOP.Customer_Employee;
 
-/**
- *
- * @author bvos
- */
+
 public class Customer {
+    /*
+    private int id;
+    private String isim;
+    private String soyIsim;
+    */
+    private String eMail;
+    
+
     
 }
--- OOP/Customer_Employee/Employee.java
+++ OOP/Customer_Employee/Employee.java
@@ -4,6 +4,9 @@ package OOP.Customer_Employee;
 
 public class Employee {
 
-
+    private int id;
+    private String isim;
+    private String soyIsim;
+    private double maas;
     
 }
