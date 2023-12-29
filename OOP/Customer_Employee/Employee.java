f9240cf3e7e5c4e8fb4c88fb9a5f0a6fbef1f7be
why20w18 <enco3162@gmail.com>
2023-12-29 17:19:47 +0300

    getter ve setter eklendi Metot eklendi

--- OOP/Customer_Employee/Employee.java
+++ OOP/Customer_Employee/Employee.java
@@ -2,11 +2,23 @@ package OOP.Customer_Employee;
 
 
 
-public class Employee {
-
+public class Employee extends Person{
+    /*
     private int id;
     private String isim;
     private String soyIsim;
+    */
     private double maas;
+
+    public double getMaas() {
+        return maas;
+    }
+
+    public void setMaas(double maas) {
+        this.maas = maas;
+    }
     
+    public void mesaiGunleri(String isim){
+        System.out.println(isim+"'in mesai gunleri Pazartesi ve Cuma");
+    }
 }
