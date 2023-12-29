488f3db55aaa6200eda075ccd28ce904130a384c
why20w18 <enco3162@gmail.com>
2023-12-29 17:20:46 +0300

    Nesneler referansa atandi inheritance uygulandi

--- OOP/Customer_Employee/Main.java
+++ OOP/Customer_Employee/Main.java
@@ -5,7 +5,19 @@ public class Main {
     
     public static void main(String[] args) {
         
+        Customer musteri = new Customer();
+        Employee calisan = new Employee();
         
+        musteri.seteMail("test@test.com");
+        musteri.indirimliUrunler();
+        System.out.println("Musteri E-Mail: "+musteri.geteMail());
+        
+        System.out.println("-----------------");
+        
+        calisan.setMaas(12.000);
+        calisan.setIsim("Ahmet");
+        calisan.mesaiGunleri(calisan.getIsim());
+        System.out.println("Calisanin Maasi: "+calisan.getMaas());
     }
     
 }
