7c2a9157faaa3afe8ac768ba2cc1389343b9582a
why20w18 <enco3162@gmail.com>
2023-12-29 17:20:00 +0300

    getter ve setter eklendi Metot eklendi

--- OOP/Customer_Employee/Customer.java
+++ OOP/Customer_Employee/Customer.java
@@ -1,13 +1,25 @@
 package OOP.Customer_Employee;
 
 
-public class Customer {
+public class Customer extends Person{
     /*
     private int id;
     private String isim;
     private String soyIsim;
     */
     private String eMail;
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+    
+       public void indirimliUrunler(){
+        System.out.println("Indirimli urun stokta yok");
+    }
     
 
     
