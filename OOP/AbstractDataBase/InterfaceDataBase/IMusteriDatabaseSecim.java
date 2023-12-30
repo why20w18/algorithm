0bcfeb25df5f3a2e418691a0d5461007ec921a71
why20w18 <enco3162@gmail.com>
2023-12-30 15:52:31 +0300

    3 abstract metod tanimlandi

---
+++ OOP/AbstractDataBase/InterfaceDataBase/IMusteriDatabaseSecim.java
@@ -0,0 +1,11 @@
+package OOP.AbstractDataBase.InterfaceDataBase;
+
+
+
+public interface IMusteriDatabaseSecim {
+
+    void musteriEkle();
+    void musteriCikart();
+    void musteriListele();
+    
+}
